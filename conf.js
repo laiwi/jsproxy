1c90438a4ae3bde3101103dd61751560a33ead0d
laiwi <77188931+laiwi@users.noreply.github.com>
2021-07-01 12:00:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,12 +2,12 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '2',
+  ver: '3',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 2
+    ver: 3
   },
 
   // 节点配置
@@ -26,6 +26,7 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
+        'jsproxy02.52f.workers.dev': 1,
         'jsproxy01.52f.workers.dev': 1,
         'jsproxy.52f.workers.dev': 1,
       }
